086bf6ef4cf441816dccfc666684e144ea1b4b10
Matheus Pintor Fernandes Ferreira <mathfer1000@gmail.com>
2026-03-03 19:19:58 -0300

    Excluir diretório do projeto
    chore: excluir diretório incorreto e aninhado do projeto

--- school/src/main/java/com/bn/school/repositories/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bn.school.repositories;
-
-import com.bn.school.models.StudentModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<StudentModel, Long> {
-
-}
