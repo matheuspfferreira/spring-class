495f6ca1371182eb41870ce807fa03467ffd5f94
Matheus Pintor Fernandes Ferreira <mathfer1000@gmail.com>
2026-03-02 22:24:41 -0300

    Adicionar gerenciamento de estudantes
    feat: adicionar gerenciamento de estudantes básico

---
+++ school/src/main/java/com/bn/school/controllers/StudentController.java
@@ -0,0 +1,31 @@
+package com.bn.school.controllers;
+
+import com.bn.school.models.StudentModel;
+import com.bn.school.services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping()
+    public List<StudentModel> findAll() {
+        return studentService.findAll();
+    }
+
+    @PostMapping
+    public StudentModel createStudent(@RequestBody StudentModel studentModel) {
+        return studentService.createStudent(studentModel);
+    }
+
+    @DeleteMapping("/{studentId}")
+    public void deleteStudent(@PathVariable Long studentId) {
+        studentService.deleteStudent(studentId);
+    }
+}
